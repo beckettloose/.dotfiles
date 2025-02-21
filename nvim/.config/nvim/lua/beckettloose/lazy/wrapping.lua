e1024e976730d38beca5222ff8b06542dcb16a8b
Beckett Loose <beckettloose@me.com>
2025-02-21 10:07:57 -0500

    feat(nvim): improve wrapping mode shortcuts

--- nvim/.config/nvim/lua/beckettloose/lazy/wrapping.lua
+++ nvim/.config/nvim/lua/beckettloose/lazy/wrapping.lua
@@ -1,6 +1,22 @@
 return {
     "andrewferrier/wrapping.nvim",
     config = function ()
-        require("wrapping").setup()
+        local opts = {
+            create_commands = true,
+            create_keymaps = false
+        }
+        require("wrapping").setup(opts)
+
+        vim.keymap.set("n", "<leader>vwh", function ()
+            require('wrapping').hard_wrap_mode()
+        end, { desc = "[H]ard Wrapping" })
+
+        vim.keymap.set("n", "<leader>vws", function ()
+            require('wrapping').soft_wrap_mode()
+        end, { desc = "[S]oft Wrapping" })
+
+        vim.keymap.set("n", "<leader>vwt", function ()
+            require('wrapping').toggle_wrap_mode()
+        end, { desc = "[T]oggle Wrapping Mode" })
     end
 }
