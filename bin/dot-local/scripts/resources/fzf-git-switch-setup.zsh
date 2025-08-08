b5e5115410a89fcf48b00214f794443e075b4fbb
Beckett Loose <beckettloose@me.com>
2025-08-08 12:20:35 -0400

    Improve fzf git switch script to work better as a zle widget

--- bin/dot-local/scripts/resources/fzf-git-switch-setup.zsh
+++ bin/dot-local/scripts/resources/fzf-git-switch-setup.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 function _fzf-git-switch {
-    printf "\n" # make sure errors don't show up on the prompt line
+    printf "\33[2K\r" # Clear the prompt line so prints are displayed better
 
     # make sure we are in a git repo
     if ! git rev-parse --is-inside-work-tree > /dev/null; then
@@ -9,44 +9,44 @@ function _fzf-git-switch {
         return
     fi
 
-    # create empty array to store branches
     branches=()
 
     if [[ $1 = "all" ]]; then
-        fzf_label="Fuzzy Git Switch (With Remotes)"
         # get all local branches without upstreams
+        fzf_label="Fuzzy Git Switch (Include Remotes)"
         printf "Finding local branches...\r"
         for i in $(git branch --format "%(refname:short) %(upstream)" | awk '{if (!$2) print $1;}'); do
             branches+=("$i");
         done
 
-        # get all branches from remotes (deduplicated)
-        printf "Finding remote branches... this may take a while (make sure all remotes are reachable)\r"
+        # get all branches from remotes (and deduplicate)
+        printf "Finding remote branches... (if this is taking a while, make sure all remotes are reachable)\r"
         for j in $(git ls-remote -q --heads | awk '{print $2;}' | sed 's/^refs\/heads\///'); do
             if [[ ! " ${branches[*]} " =~ [[:space:]]${j}[[:space:]] ]]; then
                 branches+=("${j}");
             fi
         done
     else
-        fzf_label="Fuzzy Git Switch (Local Only)"
-        # find local branches only (for performance)
+        # find local branches only (better performance since no network access)
+        fzf_label="Fuzzy Git Switch (Local Branches Only)"
         printf "Finding local branches...\r"
         for i in $(git branch --format "%(refname:short)"); do
             branches+=("$i")
         done
     fi
 
-    # send to fzf
     choice=$(printf "%s\n" "${branches[@]}" | fzf --height 40% --layout reverse --border --border-label="${fzf_label}")
 
-    # execute switch if branch was provided
     if [[ -n "$choice" ]]; then
-        # Fetch new branches from remotes.
-        printf "Fetching from remotes...\n"
-        git fetch
+        if [[ $1 = "all" ]]; then
+            # Fetch any new branches from remotes.
+            printf "Fetching from remotes...\n"
+            git fetch
+        fi
         git switch "$choice"
+        zle .reset-prompt
+        return
     else
-        echo "fatal: no branch selected"
         zle .reset-prompt
         return
     fi
--- bin/dot-local/scripts/resources/fzf-ssh-setup.zsh
+++ bin/dot-local/scripts/resources/fzf-ssh-setup.zsh
@@ -4,19 +4,17 @@
 #
 # Written by Beckett Loose
 #
-# Allows the user to quickly ssh to a specified host by fuzzy searching a list
-# from a configuration file. It is recommended to bind this to a keyboard
-# shortcut for easy access
-#
-# To Debug: DEBUG=1 fzf-ssh
+# zle widget allowing the user to quickly ssh to a specified host by fuzzy
+# searching a list from a configuration file. It is recommended to bind this
+# to a keyboard shortcut for easy access
 #
 # Currently Broken Features
-#   - validate config file before execution
-#   - use custom fzf options
 #   - handle default user name
 #   - move zle cursor back to username
 
 function _fzf-ssh {
+    printf "\33[2K\rLoading fzf-ssh...\r"
+
     # Set default config file location if not already set
     if [[ -z $FUZZY_SSH_CONFIG_FILE ]]; then
         FUZZY_SSH_CONFIG_FILE="$HOME/.fzf-ssh.yaml"
@@ -27,33 +25,47 @@ function _fzf-ssh {
     elif [[ -L $FUZZY_SSH_CONFIG_FILE ]]; then
         :
     else
-        echo "fatal: specified config file does not exist!"
+        echo "\nfatal: specified config file does not exist!"
         zle .reset-prompt
         return
     fi
 
     # Check that yq exists
     if ! type "yq" > /dev/null; then
-        echo "fatal: yq not in current path! is it installed?"
+        echo "\nfatal: yq not in current path! is it installed?"
         zle .reset-prompt
         return
     fi
 
     # Check that fzf exists
     if ! type "fzf" > /dev/null; then
-        echo "fatal: fzf not in current path! is it installed?"
+        echo "\nfatal: fzf not in current path! is it installed?"
         zle .reset-prompt
         return
     fi
 
-    fzf_options=$(yq -r ".config.fzf_options.[]" $FUZZY_SSH_CONFIG_FILE | tr '\n' ' ')
+    # Make sure the 'hosts' key exists in the config file
+    if [[ "$(yq '. | has("hosts")' $FUZZY_SSH_CONFIG_FILE)" == "true" ]]; then
+        :
+    else
+        echo "\nfatal: config file does not contain top level key 'hosts'"
+        zle .reset-prompt
+        return
+    fi
+
+    # If the user provided custom fzf options, apply them
+    if [[ "$(yq '.config | has("fzf_options")' $FUZZY_SSH_CONFIG_FILE)" == "true" ]]; then
+        fzf_options=$(yq -r ".config.fzf_options.[]" $FUZZY_SSH_CONFIG_FILE | tr '\n' ' ')
+    else
+        fzf_options="--height 40% --layout reverse --border --border-label=\"Fuzzy SSH\""
+    fi
 
     # get hosts and send to fzf
-    choice=$(yq -r ".hosts.[]" $FUZZY_SSH_CONFIG_FILE | FZF_DEFAULT_OPTS="--height 40% --layout reverse --border --border-label=\"Fuzzy SSH\"" fzf)
+    # choice=$(yq -r ".hosts.[]" $FUZZY_SSH_CONFIG_FILE | FZF_DEFAULT_OPTS="--height 40% --layout reverse --border --border-label=\"Fuzzy SSH\"" fzf)
+    choice=$(yq ".hosts.[]" $FUZZY_SSH_CONFIG_FILE | FZF_DEFAULT_OPTS="$fzf_options" fzf)
 
     # make sure we selected something
     if ! [[ -n "$choice" ]]; then
-        echo "fatal: no host selected"
         zle .reset-prompt
         return
     fi
@@ -61,8 +73,8 @@ function _fzf-ssh {
     instant=$(yq ".config.defaults.instant" $FUZZY_SSH_CONFIG_FILE)
 
     if [[ $instant == "true" ]]; then
-        echo "Selected Host: $choice"
         zle .kill-whole-line
+        zle .reset-prompt
         BUFFER="ssh $choice"
         zle .accept-line
         return
