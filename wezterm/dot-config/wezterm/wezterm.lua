7a3ab50f404c90628a6ae24d374ad8aaffe73830
Beckett Loose <beckettloose@me.com>
2026-01-14 21:04:56 -0500

    Add basic wezterm configuration

---
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -0,0 +1,43 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 200
+config.initial_rows = 40
+
+-- or, changing the font size and color scheme.
+config.font_size = 14
+config.color_scheme = 'Tokyo Night'
+
+-- disable window padding
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0
+}
+
+-- force window size in integer character increments
+config.use_resize_increments = true
+
+-- tmux can handle this
+config.enable_tab_bar = false
+
+-- Disable pane splitting by overriding default key bindings (thanks ChatGPT)
+config.keys = {
+  -- Horizontal splits
+  { key = '"', mods = 'CTRL|SHIFT', action = wezterm.action.DisableDefaultAssignment },
+  { key = '%', mods = 'CTRL|SHIFT', action = wezterm.action.DisableDefaultAssignment },
+
+  -- Some people bind splits on ALT or SUPER
+  { key = 'v', mods = 'ALT', action = wezterm.action.DisableDefaultAssignment },
+  { key = 's', mods = 'ALT', action = wezterm.action.DisableDefaultAssignment },
+}
+
+-- Finally, return the configuration to wezterm:
+return config
